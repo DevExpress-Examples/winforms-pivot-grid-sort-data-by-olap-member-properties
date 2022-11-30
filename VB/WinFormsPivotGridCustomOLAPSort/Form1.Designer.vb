51474e9a9ea94aa1d9211088749d23d1d32e4bc7
DevExpressExampleBot <fakeEmail>
2022-11-30 15:16:47 +0400

    Source auto update [skip ci]

--- VB/WinFormsPivotGridCustomOLAPSort/Form1.Designer.vb
+++ VB/WinFormsPivotGridCustomOLAPSort/Form1.Designer.vb
@@ -1,95 +1,95 @@
-﻿Namespace WinFormsPivotGridCustomOLAPSort
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WinFormsPivotGridCustomOLAPSort
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.fieldProduct = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			Me.fieldInternetSalesAmount = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldFiscalYear = New DevExpress.XtraPivotGrid.PivotGridField()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' fieldProduct
-			' 
-			Me.fieldProduct.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldProduct.AreaIndex = 0
-			Me.fieldProduct.Caption = "Product"
-			Me.fieldProduct.FieldName = "[Product].[Product].[Product]"
-			Me.fieldProduct.Name = "fieldProduct"
-			' 
-			' pivotGridControl1
-			' 
-			Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldInternetSalesAmount, Me.fieldFiscalYear, Me.fieldProduct})
-			Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.OLAPConnectionString = "provider=MSOLAP;data source=http://demos.devexpress.com/Services/OLAP/msmdpump.dl" & "l;initial catalog=""Adventure Works DW Standard Edition"";cube name=""Adventure Wor" & "ks"""
-			Me.pivotGridControl1.OLAPDataProvider = DevExpress.XtraPivotGrid.OLAPDataProvider.Adomd
-			Me.pivotGridControl1.Size = New System.Drawing.Size(719, 466)
-			Me.pivotGridControl1.TabIndex = 0
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.pivotGridControl1.CustomServerModeSort += new System.EventHandler<DevExpress.XtraPivotGrid.CustomServerModeSortEventArgs>(this.pivotGridControl1_CustomServerModeSort);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.pivotGridControl1.FieldValueDisplayText += new DevExpress.XtraPivotGrid.PivotFieldDisplayTextEventHandler(this.pivotGridControl1_FieldValueDisplayText);
-			' 
-			' fieldInternetSalesAmount
-			' 
-			Me.fieldInternetSalesAmount.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldInternetSalesAmount.AreaIndex = 0
-			Me.fieldInternetSalesAmount.Caption = "Internet Sales Amount"
-			Me.fieldInternetSalesAmount.DisplayFolder = "Internet Sales"
-			Me.fieldInternetSalesAmount.FieldName = "[Measures].[Internet Sales Amount]"
-			Me.fieldInternetSalesAmount.Name = "fieldInternetSalesAmount"
-			' 
-			' fieldFiscalYear
-			' 
-			Me.fieldFiscalYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldFiscalYear.AreaIndex = 0
-			Me.fieldFiscalYear.Caption = "Fiscal Year"
-			Me.fieldFiscalYear.DisplayFolder = "Fiscal"
-			Me.fieldFiscalYear.FieldName = "[Date].[Fiscal Year].[Fiscal Year]"
-			Me.fieldFiscalYear.Name = "fieldFiscalYear"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(719, 466)
-			Me.Controls.Add(Me.pivotGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Custom OLAP Sorting"
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.fieldProduct = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            Me.fieldInternetSalesAmount = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldFiscalYear = New DevExpress.XtraPivotGrid.PivotGridField()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' fieldProduct
+            ' 
+            Me.fieldProduct.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldProduct.AreaIndex = 0
+            Me.fieldProduct.Caption = "Product"
+            Me.fieldProduct.FieldName = "[Product].[Product].[Product]"
+            Me.fieldProduct.Name = "fieldProduct"
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldInternetSalesAmount, Me.fieldFiscalYear, Me.fieldProduct})
+            Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.OLAPConnectionString = "provider=MSOLAP;data source=http://demos.devexpress.com/Services/OLAP/msmdpump.dl" & "l;initial catalog=""Adventure Works DW Standard Edition"";cube name=""Adventure Wor" & "ks"""
+            Me.pivotGridControl1.OLAPDataProvider = DevExpress.XtraPivotGrid.OLAPDataProvider.Adomd
+            Me.pivotGridControl1.Size = New System.Drawing.Size(719, 466)
+            Me.pivotGridControl1.TabIndex = 0
+            AddHandler Me.pivotGridControl1.CustomServerModeSort, New System.EventHandler(Of DevExpress.XtraPivotGrid.CustomServerModeSortEventArgs)(AddressOf Me.pivotGridControl1_CustomServerModeSort)
+            AddHandler Me.pivotGridControl1.FieldValueDisplayText, New DevExpress.XtraPivotGrid.PivotFieldDisplayTextEventHandler(AddressOf Me.pivotGridControl1_FieldValueDisplayText)
+            ' 
+            ' fieldInternetSalesAmount
+            ' 
+            Me.fieldInternetSalesAmount.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldInternetSalesAmount.AreaIndex = 0
+            Me.fieldInternetSalesAmount.Caption = "Internet Sales Amount"
+            Me.fieldInternetSalesAmount.DisplayFolder = "Internet Sales"
+            Me.fieldInternetSalesAmount.FieldName = "[Measures].[Internet Sales Amount]"
+            Me.fieldInternetSalesAmount.Name = "fieldInternetSalesAmount"
+            ' 
+            ' fieldFiscalYear
+            ' 
+            Me.fieldFiscalYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldFiscalYear.AreaIndex = 0
+            Me.fieldFiscalYear.Caption = "Fiscal Year"
+            Me.fieldFiscalYear.DisplayFolder = "Fiscal"
+            Me.fieldFiscalYear.FieldName = "[Date].[Fiscal Year].[Fiscal Year]"
+            Me.fieldFiscalYear.Name = "fieldFiscalYear"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(719, 466)
+            Me.Controls.Add(Me.pivotGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Custom OLAP Sorting"
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private fieldInternetSalesAmount As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldFiscalYear As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldProduct As DevExpress.XtraPivotGrid.PivotGridField
-	End Class
+#End Region
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+
+        Private fieldInternetSalesAmount As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldFiscalYear As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldProduct As DevExpress.XtraPivotGrid.PivotGridField
+    End Class
 End Namespace
-
--- VB/WinFormsPivotGridCustomOLAPSort/Form1.vb
+++ VB/WinFormsPivotGridCustomOLAPSort/Form1.vb
@@ -1,40 +1,37 @@
-﻿Imports System.Windows.Forms
+Imports System.Windows.Forms
 Imports DevExpress.XtraPivotGrid
-Imports DevExpress.Data.Filtering
 Imports System.Collections
 Imports System.Linq
-Imports System
 
 Namespace WinFormsPivotGridCustomOLAPSort
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
+    Public Partial Class Form1
+        Inherits Form
 
-			InitializeComponent()
-			' Creates a new collection of OLAP member properties.
-			fieldProduct.AutoPopulatedProperties = New String() { "Color", "Class", "List Price" }
-			'Sets a field's sort mode to Custom to raise the CustomServerModeSort event.
-			fieldProduct.SortMode = PivotSortMode.Custom
-			fieldFiscalYear.FilterValues.FilterType = PivotFilterType.Included
-			fieldFiscalYear.FilterValues.ValuesIncluded = New Object() { 2004, 2005 }
-			pivotGridControl1.BestFit()
+        Public Sub New()
+            InitializeComponent()
+            ' Creates a new collection of OLAP member properties.
+            fieldProduct.AutoPopulatedProperties = New String() {"Color", "Class", "List Price"}
+            'Sets a field's sort mode to Custom to raise the CustomServerModeSort event.
+            fieldProduct.SortMode = PivotSortMode.Custom
+            fieldFiscalYear.FilterValues.FilterType = PivotFilterType.Included
+            fieldFiscalYear.FilterValues.ValuesIncluded = New Object() {2004, 2005}
+            pivotGridControl1.BestFit()
+        End Sub
 
-		End Sub
+        Private Sub pivotGridControl1_CustomServerModeSort(ByVal sender As Object, ByVal e As CustomServerModeSortEventArgs)
+            If e.Field Is fieldProduct Then
+                ' Sets the result of comparing the "Product" field's values 
+                ' by the "Color" OLAP member property.
+                e.Result = Comparer.Default.Compare(e.OLAPMember1.AutoPopulatedProperties("List Price").Value, e.OLAPMember2.AutoPopulatedProperties("List Price").Value)
+            End If
+        End Sub
 
-		Private Sub pivotGridControl1_CustomServerModeSort(ByVal sender As Object, ByVal e As CustomServerModeSortEventArgs) Handles pivotGridControl1.CustomServerModeSort
-			If e.Field Is fieldProduct Then
-				' Sets the result of comparing the "Product" field's values 
-				' by the "Color" OLAP member property.
-				e.Result = Comparer.Default.Compare(e.OLAPMember1.AutoPopulatedProperties("List Price").Value, e.OLAPMember2.AutoPopulatedProperties("List Price").Value)
-			End If
-		End Sub
-
-		Private Sub pivotGridControl1_FieldValueDisplayText(ByVal sender As Object, ByVal e As PivotFieldDisplayTextEventArgs) Handles pivotGridControl1.FieldValueDisplayText
-			If e.Field Is fieldProduct Then
-				Dim currentMember As IOLAPMember = e.Field.GetOLAPMembers().First(Function(m) Object.Equals(m.Value, e.Value))
-				e.DisplayText &= String.Format(" ({0:C2})", currentMember.AutoPopulatedProperties("List Price").Value)
-			End If
-		End Sub
-	End Class
+        Private Sub pivotGridControl1_FieldValueDisplayText(ByVal sender As Object, ByVal e As PivotFieldDisplayTextEventArgs)
+            If e.Field Is fieldProduct Then
+                Dim currentMember As IOLAPMember = e.Field.GetOLAPMembers().First(Function(m) Equals(m.Value, e.Value))
+                e.DisplayText += String.Format(" ({0:C2})", currentMember.AutoPopulatedProperties("List Price").Value)
+            End If
+        End Sub
+    End Class
 End Namespace
